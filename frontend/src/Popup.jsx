85104e45d42e740cc36182d6c0b9e8c3df61f08d
Cassandra McCormack <cassandra.m.mccormack@gmail.com>
2025-08-01 22:02:12 +0100

    styling improvements

--- frontend/src/Popup.jsx
+++ frontend/src/Popup.jsx
@@ -1,46 +1,38 @@
 import Popup from 'reactjs-popup';
 import './Popup.css'
 import { AddCharacter } from '../wailsjs/go/main/App';
+import FormStats from './FormStats';
+
+const currentPrefix = 'current_';
+const targetPrefix = 'target_';
 
 const handleSubmit = (event) => {
   event.preventDefault();
-  let name = event.target.name.value
-  let element = event.target.element.value
-  let level = event.target.level.value
-  let hp = event.target.hp.value
-  let atk = event.target.atk.value
-  let def = event.target.def.value
-  let critRate = event.target.critRate.value
-  let critDmg = event.target.critDmg.value
-  let energyRecharge = event.target.energyRecharge.value
-  let em = event.target.em.value
-  let physicalDmgBonus = event.target.physicalDmgBonus.value
-  let elementalDmgBonus = event.target.elementalDmgBonus.value
   let character = {
-    name: name, 
-    element: element, 
-    level: parseInt(level), 
+    name: event.target.name.value,
+    element: event.target.element.value,
+    level: parseInt(event.target.level.value),
     current_stats: {
-      hp: parseFloat(hp), 
-      attack: parseFloat(atk), 
-      defense: parseFloat(def), 
-      crit_rate: parseFloat(critRate), 
-      crit_damage: parseFloat(critDmg), 
-      energy_recharge: parseFloat(energyRecharge), 
-      elemental_mastery: parseFloat(em), 
-      physical_damage_bonus: parseFloat(physicalDmgBonus), 
-      elemental_damage_bonus: parseFloat(elementalDmgBonus)
+      hp: parseFloat(event.target[`${currentPrefix}hp`].value),
+      attack: parseFloat(event.target[`${currentPrefix}atk`].value),
+      defense: parseFloat(event.target[`${currentPrefix}def`].value),
+      crit_rate: parseFloat(event.target[`${currentPrefix}critRate`].value),
+      crit_damage: parseFloat(event.target[`${currentPrefix}critDmg`].value),
+      energy_recharge: parseFloat(event.target[`${currentPrefix}energyRecharge`].value),
+      elemental_mastery: parseFloat(event.target[`${currentPrefix}em`].value),
+      physical_damage_bonus: parseFloat(event.target[`${currentPrefix}physicalDmgBonus`].value),
+      elemental_damage_bonus: parseFloat(event.target[`${currentPrefix}elementalDmgBonus`].value)
     },
     target_stats: {
-      hp: parseFloat(event.target.target_hp.value),
-      attack: parseFloat(event.target.target_atk.value),
-      defense: parseFloat(event.target.target_def.value),
-      crit_rate: parseFloat(event.target.target_critRate.value),
-      crit_damage: parseFloat(event.target.target_critDmg.value),
-      energy_recharge: parseFloat(event.target.target_energyRecharge.value),
-      elemental_mastery: parseFloat(event.target.target_em.value),
-      physical_damage_bonus: parseFloat(event.target.target_physicalDmgBonus.value),
-      elemental_damage_bonus: parseFloat(event.target.target_elementalDmgBonus.value)
+      hp: parseFloat(event.target[`${targetPrefix}hp`].value),
+      attack: parseFloat(event.target[`${targetPrefix}atk`].value),
+      defense: parseFloat(event.target[`${targetPrefix}def`].value),
+      crit_rate: parseFloat(event.target[`${targetPrefix}critRate`].value),
+      crit_damage: parseFloat(event.target[`${targetPrefix}critDmg`].value),
+      energy_recharge: parseFloat(event.target[`${targetPrefix}energyRecharge`].value),
+      elemental_mastery: parseFloat(event.target[`${targetPrefix}em`].value),
+      physical_damage_bonus: parseFloat(event.target[`${targetPrefix}physicalDmgBonus`].value),
+      elemental_damage_bonus: parseFloat(event.target[`${targetPrefix}elementalDmgBonus`].value)
     }
   }
 
@@ -55,95 +47,44 @@ function addCharacter({character}) {
   })
 }
 
+function createElementRadio() {
+  const elements = ['anemo', 'geo', 'electro', 'dendro', 'hydro', 'pyro', 'cryo'];
+  return elements.map((element) => (
+      <div key={element}>
+        <input type="radio" name="element" value={element} id={element}/>
+        <label htmlFor={element}>{element.charAt(0).toUpperCase() + element.slice(1)}</label>
+      </div>
+  ));
+}
+
+
 export default ({ text }) => (
 
   <Popup trigger={<button> {text}</button>} modal>
-      <form onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         
-        <label htmlFor='name'>Name</label>
-        <input type="text" id="name"/>
+        <div className='form-header'>
+          <h3>Add Character</h3>
+          <label htmlFor='name'>Name</label>
+          <input type="text" id="name"/>
 
-        <label htmlFor='level'>Level</label>
-        <input type="number" id="level" min="1" max="90"/>
+          <label htmlFor='level'>Level</label>
+          <input type="number" id="level" min="1" max="90"/>
 
-        <label>
-          Element
-          <input type="radio" name="element" value="anemo"/>
-          <label>Anemo</label>
-          <input type="radio" name="element" value="geo"/>
-          <label>Geo</label>
-          <input type="radio" name="element" value="electro"/>
-          <label>Electro</label>
-          <input type="radio" name="element" value="dendro"/>
-          <label>Dendro</label>   
-          <input type="radio" name="element" value="hydro"/>
-          <label>Hydro</label>
-          <input type="radio" name="element" value="pyro"/>
-          <label>Pyro</label>
-          <input type="radio" name="element" value="cryo"/>
-          <label>Cryo</label>
-        </label>
-
-        <div>
-          <h3>Current Stats</h3>
-          <label htmlFor='hp'>HP</label>
-          <input type="number" id="hp" min="0"/>
-
-          <label htmlFor='atk'>ATK</label>
-          <input type="number" id="atk" min="0"/>
-
-          <label htmlFor='def'>DEF</label>
-          <input type="number" id="def" min="0"/>
-
-          <label htmlFor='critRate'>Crit Rate</label>
-          <input type="number" id="critRate" min="0"/>
-
-          <label htmlFor='critDmg'>Crit Damage</label>
-          <input type="number" id="critDmg" min="0"/>
-
-          <label htmlFor='energyRecharge'>Energy Recharge</label>
-          <input type="number" id="energyRecharge" min="0"/>
-
-          <label htmlFor='em'>Elemental Mastery</label>
-          <input type="number" id="em" min="0"/>
-
-          <label htmlFor='physicalDmgBonus'>Physical DMG Bonus</label>
-          <input type="number" id="physicalDmgBonus" min="0"/>
-
-          <label htmlFor='elementalDmgBonus'>Elemental DMG Bonus</label>
-          <input type="number" id="elementalDmgBonus" min="0"/>
-        </div>
-        <div>
-          <h3>Target Stats</h3>
-          <label htmlFor='target_hp'>Target HP</label>
-          <input type="number" id="target_hp" min="0"/>
-          
-          <label htmlFor='target_atk'>Target ATK</label>
-          <input type="number" id="target_atk" min="0"/>
-
-          <label htmlFor='target_def'>Target DEF</label>
-          <input type="number" id="target_def" min="0"/>
-
-          <label htmlFor='target_critRate'>Target Crit Rate</label>
-          <input type="number" id="target_critRate" min="0"/>
-
-          <label htmlFor='target_critDmg'>Target Crit Damage</label>
-          <input type="number" id="target_critDmg" min="0"/>
-
-          <label htmlFor='target_energyRecharge'>Target Energy Recharge</label>
-          <input type="number" id="target_energyRecharge" min="0"/>
-
-          <label htmlFor='target_em'>Target Elemental Mastery</label>
-          <input type="number" id="target_em" min="0"/>
-
-          <label htmlFor='target_physicalDmgBonus'>Target Physical DMG Bonus</label>
-          <input type="number" id="target_physicalDmgBonus" min="0"/>
-
-          <label htmlFor='target_elementalDmgBonus'>Target Elemental DMG Bonus</label>
-          <input type="number" id="target_elementalDmgBonus" min="0"/>
+          <div className='form-submit'>
+            <button type="submit">Submit</button>
+          </div>
         </div>
 
-        <button type="submit">Submit</button>
+        <div className='form-elements'>
+          <h3>Element</h3>
+          {createElementRadio()}
+        </div>
+
+        <FormStats title="Current Stats" idPrefix={`${currentPrefix}`}/>
+        <FormStats title="Target Stats" idPrefix={`${targetPrefix}`}/>
+
+
       
     </form>
   </Popup>
