122efca4fb42b9cfb84111e3baedc021d0395aa4
cassandramcc <cassandra.m.mccormack@gmail.com>
2025-08-01 20:23:09 +0100

    get add character form working

--- frontend/src/Popup.jsx
+++ frontend/src/Popup.jsx
@@ -1,35 +1,50 @@
 import Popup from 'reactjs-popup';
 import './Popup.css'
+import { AddCharacter } from '../wailsjs/go/main/App';
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  let name = event.target.name.value
+  let element = event.target.element.value
+  addCharacter({character: {name: name, element: element}})
+}
+
+function addCharacter({character}) {
+  AddCharacter(character).then(() => {
+    alert('Character added successfully!')
+  }).catch((error) => {
+    alert('Failed to add character: ' + error.message);
+  })
+}
 
 export default ({ text }) => (
+
   <Popup trigger={<button> {text}</button>} modal>
-    <div>
-        <form className='form'>
-            <label>
-                Name
-                <input type="text"/>
-            </label>
+      <form onSubmit={handleSubmit}>
+        
+        <label htmlFor='name'>Name</label>
+        <input type="text" id="name"/>
 
-            <label>
-                Element
-                <input type="radio" value="Anemo" name="element"/>
-                <label>Anemo</label>
-                <input type="radio" name="element" value="Geo"/>
-                <label>Geo</label>
-                <input type="radio" name="element" value="Electro"/>
-                <label>Electro</label>
-                <input type="radio" name="element" value="Dendro"/>
-                <label>Dendro</label>   
-                <input type="radio" name="element" value="Hydro"/>
-                <label>Hydro</label>
-                <input type="radio" name="element" value="Pyro"/>
-                <label>Pyro</label>
-                <input type="radio" name="element" value="Cryo"/>
-                <label>Cryo</label>
-            </label>
+        <label>
+          Element
+          <input type="radio" name="element" value="anemo"/>
+          <label>Anemo</label>
+          <input type="radio" name="element" value="geo"/>
+          <label>Geo</label>
+          <input type="radio" name="element" value="electro"/>
+          <label>Electro</label>
+          <input type="radio" name="element" value="dendro"/>
+          <label>Dendro</label>   
+          <input type="radio" name="element" value="hydro"/>
+          <label>Hydro</label>
+          <input type="radio" name="element" value="pyro"/>
+          <label>Pyro</label>
+          <input type="radio" name="element" value="cryo"/>
+          <label>Cryo</label>
+        </label>
 
-            <input type="submit" value="Submit"/>
-        </form> 
-    </div>
+      <button type="submit">Submit</button>
+      
+    </form>
   </Popup>
 );
--- frontend/src/Titlebar.jsx
+++ frontend/src/Titlebar.jsx
@@ -1,23 +1,7 @@
-import Button from './Button'
 import './Titlebar.css'
-import {AddCharacter} from "../wailsjs/go/main/App";
 import Popup from './Popup';
 
 function Titlebar() {
-
-    function openForm() {
-        document.getElementById("characterForm").style.display = "block";
-    }
-
-    function addCharacter({character}) {
-        AddCharacter(character).then(() => {
-            alert('Character added successfully!')
-        }).catch((error) => {
-            alert('Failed to add character: ' + error.message);
-        })
-    }
-
-
     return (
         <div className="titlebar">
             <Popup text={"Add Character"}/>
@@ -25,7 +9,6 @@ function Titlebar() {
                 <img src="/assets/images/icon.png"/>
                 <h1>Genshin Dashboard</h1>
             </div>
-            <Button text="Add Character" onClick={() => addCharacter({character: {name: 'New Character'}})} />
         </div>
     )
 }
