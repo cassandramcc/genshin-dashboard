1d6255df53066141e1d33772375e728662356f3a
Cassandra McCormack <cassandra.m.mccormack@gmail.com>
2025-08-05 21:20:45 +0100

    add some stats

--- frontend/src/FormStats.jsx
+++ frontend/src/FormStats.jsx
@@ -3,31 +3,31 @@ export default ({title, idPrefix}) => {
         <div className="form-stats">
           <h3>{title}</h3>
           <label htmlFor={`${idPrefix}hp`}>HP</label>
-          <input type="number" id={`${idPrefix}hp`} min="0"/>
+          <input type="number" step="0.1" id={`${idPrefix}hp`} min="0"/>
 
           <label htmlFor={`${idPrefix}atk`}>ATK</label>
-          <input type="number" id={`${idPrefix}atk`} min="0"/>
+          <input type="number" step="0.1" id={`${idPrefix}atk`} min="0"/>
 
           <label htmlFor={`${idPrefix}def`}>DEF</label>
-          <input type="number" id={`${idPrefix}def`} min="0"/>
+          <input type="number" step="0.1" id={`${idPrefix}def`} min="0"/>
 
           <label htmlFor={`${idPrefix}critRate`}>Crit Rate</label>
-          <input type="number" id={`${idPrefix}critRate`} min="0"/>
+          <input type="number" step="0.1" id={`${idPrefix}critRate`} min="0"/>
 
           <label htmlFor={`${idPrefix}critDmg`}>Crit Damage</label>
-          <input type="number" id={`${idPrefix}critDmg`} min="0"/>
+          <input type="number" step="0.1" id={`${idPrefix}critDmg`} min="0"/>
 
           <label htmlFor={`${idPrefix}energyRecharge`}>Energy Recharge</label>
-          <input type="number" id={`${idPrefix}energyRecharge`} min="0"/>
+          <input type="number" step="0.1" id={`${idPrefix}energyRecharge`} min="0"/>
 
           <label htmlFor={`${idPrefix}em`}>Elemental Mastery</label>
-          <input type="number" id={`${idPrefix}em`} min="0"/>
+          <input type="number" step="0.1" id={`${idPrefix}em`} min="0"/>
 
           <label htmlFor={`${idPrefix}physicalDmgBonus`}>Physical DMG Bonus</label>
-          <input type="number" id={`${idPrefix}physicalDmgBonus`} min="0"/>
+          <input type="number" step="0.1" id={`${idPrefix}physicalDmgBonus`} min="0"/>
 
           <label htmlFor={`${idPrefix}elementalDmgBonus`}>Elemental DMG Bonus</label>
-          <input type="number" id={`${idPrefix}elementalDmgBonus`} min="0"/>
+          <input type="number" step="0.1" id={`${idPrefix}elementalDmgBonus`} min="0"/>
         </div>
     );
 }
