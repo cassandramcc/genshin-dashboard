926c39fa90dd8ab27326e93c0534169a5820a22a
Cassandra McCormack <cassandra.m.mccormack@gmail.com>
2025-08-11 23:27:14 +0100

    linting

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: globals.node },
+    rules: { "react/react-in-jsx-scope": "off" }
+  },
+  pluginReact.configs.flat.recommended,
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);
